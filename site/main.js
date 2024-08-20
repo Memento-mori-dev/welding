82f323680fcc6fe46344e4e25fe9f12327d2eba6
Kirill Pet <job.mori.1967@gmail.com>
2024-08-20 15:04:36 +0300

    сделал адаптивность header

--- site/main.js
+++ site/main.js
@@ -17,3 +17,23 @@ lang.onclick = function (e) {
         content.style.cssText = `height: ${newHeight}px`;
     }
 }
+
+let menu = document.getElementById('menu');
+
+menu.onclick = function (e) {
+    let menuContent = document.getElementById('menu-content');
+
+    let height = window.getComputedStyle(menuContent.querySelector('.menu-content')).height;
+    let newHeight = Number(height.split('px')[0]) + 25;
+
+    console.log(newHeight);
+    
+
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+        menuContent.style.cssText = 'height: 0';
+    }else{
+        menu.classList.add('active');
+        menuContent.style.cssText = `height: 100vh`;
+    }
+}
